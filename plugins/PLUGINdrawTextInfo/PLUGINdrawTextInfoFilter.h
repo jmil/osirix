4eaf44d0c1c410be8e3748579be2d7dfd7b0cdb2
jacquesfauquex <jacquesfauquex@d300eccd-f00d-0410-ae38-fa88e4616176>
2006-10-08 23:57:06 +0000

    new plugin for customized text overlay
    git-svn-id: https://osirix.svn.sourceforge.net/svnroot/osirix@1208 d300eccd-f00d-0410-ae38-fa88e4616176

---
+++ plugins/PLUGINdrawTextInfo/PLUGINdrawTextInfoFilter.h
@@ -0,0 +1,19 @@
+//
+//  PLUGINdrawTextInfoFilter.h
+//  PLUGINdrawTextInfo
+//
+//  Copyright (c) 2006 opendicom.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PluginFilter.h"
+
+@interface PLUGINdrawTextInfoFilter : PluginFilter {
+
+}
+
+- (void) initPlugin;
+
+- (void) PLUGINdrawTextInfoFilter:(NSNotification*)note;
+
+@end
