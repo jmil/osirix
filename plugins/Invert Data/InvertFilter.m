f708552d72413e4a0a0d6810c44b7a08b302cdc5
rossetantoine <rossetantoine@d300eccd-f00d-0410-ae38-fa88e4616176>
2006-10-25 15:49:19 +0000

    misc bugs
    git-svn-id: https://osirix.svn.sourceforge.net/svnroot/osirix@1351 d300eccd-f00d-0410-ae38-fa88e4616176

--- plugins/Invert Data/InvertFilter.m
+++ plugins/Invert Data/InvertFilter.m
@@ -12,75 +12,79 @@
 
 - (long) filterImage:(NSString*) menuName
 {
-	long			i, x, y;
+	long			i, x, y, z;
 	float			*fImage;
 	unsigned char   *rgbImage;
-	
-	// Contains a list of DCMPix objects: they contain the pixels of current series
-	NSArray		*pixList = [viewerController pixList];		
-	DCMPix		*curPix;
+
 	
 	// Display a waiting window
 	id waitWindow = [viewerController startWaitWindow:@"Inverting..."];
 	
-	// Loop through all images contained in the current series
-	for( i = 0; i < [pixList count]; i++)
+	for( z = 0; z < [viewerController maxMovieIndex]; z++)
 	{
-		curPix = [pixList objectAtIndex: i];
+		// Contains a list of DCMPix objects: they contain the pixels of current series
+		NSArray		*pixList = [viewerController pixList: z];		
+		DCMPix		*curPix;
 		
-		if( i == [[viewerController imageView] curImage])
+		// Loop through all images contained in the current series
+		for( i = 0; i < [pixList count]; i++)
 		{
-			NSLog(@"Cool, this is the image (%d) currently displayed!", i);
-		}
-		
-		// fImage is a pointer on the pixels, ALWAYS represented in float (float*) or in ARGB (unsigned char*) 
-		
-		if( [curPix isRGB])
-		{
-			rgbImage = (unsigned char*) [curPix fImage];
+			curPix = [pixList objectAtIndex: i];
 			
-			x = [curPix pheight] * [curPix pwidth] / 4;
-			
-			while ( x-- > 0)
+			if( i == [[viewerController imageView] curImage])
 			{
-				rgbImage++;
-				*rgbImage = 255-*rgbImage;		rgbImage++;
-				*rgbImage = 255-*rgbImage;		rgbImage++;
-				*rgbImage = 255-*rgbImage;		rgbImage++;
-				
-				rgbImage++;
-				*rgbImage = 255-*rgbImage;		rgbImage++;
-				*rgbImage = 255-*rgbImage;		rgbImage++;
-				*rgbImage = 255-*rgbImage;		rgbImage++;
-				
-				rgbImage++;
-				*rgbImage = 255-*rgbImage;		rgbImage++;
-				*rgbImage = 255-*rgbImage;		rgbImage++;
-				*rgbImage = 255-*rgbImage;		rgbImage++;
-				
-				rgbImage++;
-				*rgbImage = 255-*rgbImage;		rgbImage++;
-				*rgbImage = 255-*rgbImage;		rgbImage++;
-				*rgbImage = 255-*rgbImage;		rgbImage++;
+				NSLog(@"Cool, this is the image (%d) currently displayed!", i);
 			}
-		}
-		else
-		{
 			
-			fImage = [curPix fImage];
+			// fImage is a pointer on the pixels, ALWAYS represented in float (float*) or in ARGB (unsigned char*) 
 			
-			x = [curPix pheight] * [curPix pwidth]/4;
-			
-			while ( x-- > 0)
+			if( [curPix isRGB])
 			{
-				*fImage = -*fImage;
-				fImage++;
-				*fImage = -*fImage;
-				fImage++;
-				*fImage = -*fImage;
-				fImage++;
-				*fImage = -*fImage;
-				fImage++;
+				rgbImage = (unsigned char*) [curPix fImage];
+				
+				x = [curPix pheight] * [curPix pwidth] / 4;
+				
+				while ( x-- > 0)
+				{
+					rgbImage++;
+					*rgbImage = 255-*rgbImage;		rgbImage++;
+					*rgbImage = 255-*rgbImage;		rgbImage++;
+					*rgbImage = 255-*rgbImage;		rgbImage++;
+					
+					rgbImage++;
+					*rgbImage = 255-*rgbImage;		rgbImage++;
+					*rgbImage = 255-*rgbImage;		rgbImage++;
+					*rgbImage = 255-*rgbImage;		rgbImage++;
+					
+					rgbImage++;
+					*rgbImage = 255-*rgbImage;		rgbImage++;
+					*rgbImage = 255-*rgbImage;		rgbImage++;
+					*rgbImage = 255-*rgbImage;		rgbImage++;
+					
+					rgbImage++;
+					*rgbImage = 255-*rgbImage;		rgbImage++;
+					*rgbImage = 255-*rgbImage;		rgbImage++;
+					*rgbImage = 255-*rgbImage;		rgbImage++;
+				}
+			}
+			else
+			{
+				
+				fImage = [curPix fImage];
+				
+				x = [curPix pheight] * [curPix pwidth]/4;
+				
+				while ( x-- > 0)
+				{
+					*fImage = -*fImage;
+					fImage++;
+					*fImage = -*fImage;
+					fImage++;
+					*fImage = -*fImage;
+					fImage++;
+					*fImage = -*fImage;
+					fImage++;
+				}
 			}
 		}
 	}
@@ -89,12 +93,17 @@
 	[viewerController endWaitWindow: waitWindow];
 	
 	// Update the current displayed WL & WW : we just inverted the image -> invert the WL !
-	float wl, ww;
+	{
+		NSArray		*pixList = [viewerController pixList];
+		DCMPix		*curPix = [pixList objectAtIndex: 0];
+		
+		float wl, ww;
 	
-	[[viewerController imageView] getWLWW: &wl :&ww];
-	if( [curPix isRGB]) wl = 255-wl;
-	else wl = -wl;
-	[[viewerController imageView] setWLWW: wl :ww];
+		[[viewerController imageView] getWLWW: &wl :&ww];
+		if( [curPix isRGB]) wl = 255-wl;
+		else wl = -wl;
+		[[viewerController imageView] setWLWW: wl :ww];
+	}
 	
 	// We modified the pixels: OsiriX please update the display!
 	[viewerController needsDisplayUpdate];
