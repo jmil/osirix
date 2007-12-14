3e8d9741bd57c5d6bd0987b5f75fdc71975512d1
wcl_sd <wcl_sd@d300eccd-f00d-0410-ae38-fa88e4616176>
2007-12-14 15:11:00 +0000

    mouse scroll wheel and arrow keys support(missed new classes reupload)
    git-svn-id: https://osirix.svn.sourceforge.net/svnroot/osirix@3679 d300eccd-f00d-0410-ae38-fa88e4616176

---
+++ plugins/CMIV_CTA_TOOLS/CMIVDCMView.h
@@ -0,0 +1,18 @@
+//
+//  CMIVDCMView.h
+//  CMIV_CTA_TOOLS
+//
+//  Created by chuwa on 12/14/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "DCMView.h"	
+
+@interface CMIVDCMView : DCMView {
+	NSSlider* tranlateSlider;
+	NSSlider* horizontalSlider;
+}
+-(void)setTranlateSlider:(NSSlider*) aSlider;
+-(void)setHorizontalSlider:(NSSlider*) aSlider;
+@end
---
+++ plugins/CMIV_CTA_TOOLS/CMIVDCMView.m
@@ -0,0 +1,141 @@
+//
+//  CMIVDCMView.m
+//  CMIV_CTA_TOOLS
+//
+//  Created by chuwa on 12/14/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "CMIVDCMView.h"
+
+
+@implementation CMIVDCMView
+-(void)setTranlateSlider:(NSSlider*) aSlider
+{
+	tranlateSlider=aSlider;
+}
+-(void)setHorizontalSlider:(NSSlider*) aSlider
+{
+	horizontalSlider=aSlider;
+}
+- (void)scrollWheel:(NSEvent *)theEvent
+{
+	[super scrollWheel:theEvent];
+	CGFloat x,y;
+	float loc;
+	float acceleratefactor=0.1;
+	if(tranlateSlider!=nil)
+	{
+		if([theEvent modifierFlags] & NSCommandKeyMask )
+			y=[theEvent deltaX];
+		else
+			y=[theEvent deltaY];
+		if(y!=0)
+		{
+			if(y>0)
+				y=(y-0.1)*acceleratefactor+0.1;
+			else 
+				y=(y+0.1)*acceleratefactor-0.1;
+			loc=[tranlateSlider floatValue];
+			loc+=y*10;
+			while(loc>[tranlateSlider maxValue])
+				loc-=([tranlateSlider maxValue]-[tranlateSlider minValue]);
+			while(loc<[tranlateSlider minValue])
+				loc+=([tranlateSlider maxValue]-[tranlateSlider minValue]);
+			[tranlateSlider setFloatValue:loc];
+			[tranlateSlider performClick:self];
+		}
+		
+		
+	}
+	if(horizontalSlider!=nil)
+	{
+		if([theEvent modifierFlags] & NSCommandKeyMask )
+			x=[theEvent deltaY];
+		else
+			x=[theEvent deltaX];
+		if(x!=0)
+		{
+			if(x>0)
+				x=(x-0.1)*acceleratefactor+0.1;
+			else 
+				x=(x+0.1)*acceleratefactor-0.1;
+			loc=[horizontalSlider floatValue];
+			loc-=x*10;
+			while(loc>[horizontalSlider maxValue])
+				loc-=([horizontalSlider maxValue]-[horizontalSlider minValue]);
+			while(loc<[horizontalSlider minValue])
+				loc+=([horizontalSlider maxValue]-[horizontalSlider minValue]);
+			
+			[horizontalSlider setFloatValue:loc];
+			[horizontalSlider performClick:self];
+		}
+
+	}
+	if(tranlateSlider==nil&&horizontalSlider==nil)
+		[[self nextResponder] scrollWheel:theEvent];
+}
+- (void)keyDown:(NSEvent *)event
+{
+	unsigned short i=[event keyCode];
+	if((i>=123&&i<=126)||i==121||i==116)//arrows and pageup&down
+	{
+		float x=0,y=0;
+		float loc;
+		float acceleratefactor=1.0;
+		if(tranlateSlider!=nil)
+		{
+			if([event modifierFlags] &  NSAlternateKeyMask)
+				acceleratefactor=10.0;
+				
+			if( i==121)
+				y=1;
+			else if(i==116)
+				y=-1;
+			if(y!=0)
+			{
+				loc=[tranlateSlider floatValue];
+				loc+=y*acceleratefactor;
+				while(loc>[tranlateSlider maxValue])
+					loc-=([tranlateSlider maxValue]-[tranlateSlider minValue]);
+				while(loc<[tranlateSlider minValue])
+					loc+=([tranlateSlider maxValue]-[tranlateSlider minValue]);
+				[tranlateSlider setFloatValue:loc];
+				[tranlateSlider performClick:self];
+			}
+			
+			
+		}
+		if(horizontalSlider!=nil)
+		{
+			if(i==124)
+			{
+				x=1;
+			}
+			else if(i==123)
+			{
+				x=-1;
+				
+			}
+			if(x!=0)
+			{
+	
+				loc=[horizontalSlider floatValue];
+				loc+=x*acceleratefactor;
+				while(loc>[horizontalSlider maxValue])
+					loc-=([horizontalSlider maxValue]-[horizontalSlider minValue]);
+				while(loc<[horizontalSlider minValue])
+					loc+=([horizontalSlider maxValue]-[horizontalSlider minValue]);
+				
+				[horizontalSlider setFloatValue:loc];
+				[horizontalSlider performClick:self];
+			}
+			
+		}
+		if(tranlateSlider==nil&&horizontalSlider==nil)
+			[[self nextResponder] keyDown:event];
+	}
+	else
+		[super keyDown:event];
+}
+@end
