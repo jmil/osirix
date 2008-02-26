d67beaa1e1704cd907de6dec0e011215b2e939bf
rossetantoine <rossetantoine@d300eccd-f00d-0410-ae38-fa88e4616176>
2008-02-26 14:11:30 +0000

    SeriesSameOrientation plugin
    git-svn-id: https://osirix.svn.sourceforge.net/svnroot/osirix@3925 d300eccd-f00d-0410-ae38-fa88e4616176

---
+++ plugins/SeriesSameOrientation/SeriesSameOrientation.m
@@ -0,0 +1,111 @@
+//
+//  SeriesSameOrientation.m
+//  Apply same orientation to all images of a series
+//
+//  Created by rossetantoine on Wed Jun 09 2004.
+//  Copyright (c) 2004 Rosset Antoine. All rights reserved.
+//
+
+#import "SeriesSameOrientation.h"
+#include <Accelerate/Accelerate.h>
+#import "ITKTransform.h"
+
+@implementation SeriesSameOrientation
+
+- (long) filterImage:(NSString*) menuName
+{
+		DCMPix *curPix = [[viewerController pixList] objectAtIndex: [[viewerController imageView] curImage]];
+		long imageSize, size;
+		NSArray *pixList = [viewerController pixList];
+		
+		imageSize = [curPix pwidth] * [curPix pheight];
+		size = sizeof(float) * [pixList count]/2 * imageSize;
+		
+		float orientation[ 9];
+		
+		[curPix orientation: orientation];
+		
+		for( DCMPix *p in pixList)
+		{
+			float o[ 9];
+			
+			[p orientation: o];
+			
+			BOOL equal = YES;
+			for( int i = 0 ; i < 6 ; i++)
+			{
+				if( o[ i] != orientation[ i])
+					equal = NO;
+			}
+			
+			if( equal == NO)	// Change the orientation of the image according to the selected image
+			{
+				NSLog(@"SeriesSameOrientation: %d", [pixList indexOfObject: p]);
+				
+				float vectorModel[ 9], vectorSensor[ 9];
+				
+				[p orientation: vectorSensor];
+				[curPix orientation: vectorModel];
+				
+				double matrix[ 12], length;
+				
+				// No translation -> same origin, same study
+				matrix[ 9] = 0;
+				matrix[ 10] = 0;
+				matrix[ 11] = 0;
+				
+				// --
+				
+				matrix[ 0] = vectorSensor[ 0] * vectorModel[ 0] + vectorSensor[ 1] * vectorModel[ 1] + vectorSensor[ 2] * vectorModel[ 2];
+				matrix[ 1] = vectorSensor[ 0] * vectorModel[ 3] + vectorSensor[ 1] * vectorModel[ 4] + vectorSensor[ 2] * vectorModel[ 5];
+				matrix[ 2] = vectorSensor[ 0] * vectorModel[ 6] + vectorSensor[ 1] * vectorModel[ 7] + vectorSensor[ 2] * vectorModel[ 8];
+
+				length = sqrt(matrix[0]*matrix[0] + matrix[1]*matrix[1] + matrix[2]*matrix[2]);
+
+				matrix[0] = matrix[ 0] / length;
+				matrix[1] = matrix[ 1] / length;
+				matrix[2] = matrix[ 2] / length;
+
+				// --
+
+				matrix[ 3] = vectorSensor[ 3] * vectorModel[ 0] + vectorSensor[ 4] * vectorModel[ 1] + vectorSensor[ 5] * vectorModel[ 2];
+				matrix[ 4] = vectorSensor[ 3] * vectorModel[ 3] + vectorSensor[ 4] * vectorModel[ 4] + vectorSensor[ 5] * vectorModel[ 5];
+				matrix[ 5] = vectorSensor[ 3] * vectorModel[ 6] + vectorSensor[ 4] * vectorModel[ 7] + vectorSensor[ 5] * vectorModel[ 8];
+
+				length = sqrt(matrix[3]*matrix[3] + matrix[4]*matrix[4] + matrix[5]*matrix[5]);
+
+				matrix[3] = matrix[ 3] / length;
+				matrix[4] = matrix[ 4] / length;
+				matrix[5] = matrix[ 5] / length;
+				
+				// --
+				
+				matrix[6] = matrix[1]*matrix[5] - matrix[2]*matrix[4];
+				matrix[7] = matrix[2]*matrix[3] - matrix[0]*matrix[5];
+				matrix[8] = matrix[0]*matrix[4] - matrix[1]*matrix[3];
+				
+				length = sqrt(matrix[6]*matrix[6] + matrix[7]*matrix[7] + matrix[8]*matrix[8]);
+
+				matrix[6] = matrix[ 6] / length;
+				matrix[7] = matrix[ 7] / length;
+				matrix[8] = matrix[ 8] / length;
+				
+				long size;
+				
+				float *resultBuff = [ITKTransform reorient2Dimage: matrix firstObject: curPix firstObjectOriginal: p length: &size];
+				
+				memcpy( [p fImage] , resultBuff, size);
+				
+				free( resultBuff);
+				
+				[p setOrientation: orientation];
+			}
+		}
+		
+		// We modified the view: OsiriX please update the display!
+		[viewerController needsDisplayUpdate];
+
+	return 0;
+}
+
+@end
