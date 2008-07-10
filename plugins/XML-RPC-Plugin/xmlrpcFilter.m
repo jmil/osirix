14d8394bade0d990f568078a0860abef98595184
rossetantoine <rossetantoine@d300eccd-f00d-0410-ae38-fa88e4616176>
2008-07-10 15:16:55 +0000

    xml-rpc exceptions
    git-svn-id: https://osirix.svn.sourceforge.net/svnroot/osirix@4410 d300eccd-f00d-0410-ae38-fa88e4616176

--- plugins/XML-RPC-Plugin/xmlrpcFilter.m
+++ plugins/XML-RPC-Plugin/xmlrpcFilter.m
@@ -30,188 +30,204 @@
 
 - (void) OsiriXXMLRPCMessage: (NSNotification*) note
 {
-	NSMutableDictionary	*httpServerMessage = [note object];
-	
-	// ****************************************************************************************
-	
-	if( [[httpServerMessage valueForKey: @"MethodName"] isEqualToString: @"updateDICOMNode"])	//AETitle, Port, TransferSyntax
+	@try
 	{
-		NSXMLDocument *doc = [httpServerMessage valueForKey:@"NSXMLDocument"];						// We need the parameters
+		NSMutableDictionary	*httpServerMessage = [note object];
 		
-		NSError	*error = 0L;
-		NSArray *keys = [doc nodesForXPath:@"methodCall/params//member/name" error:&error];
-		NSArray *values = [doc nodesForXPath:@"methodCall/params//member/value" error:&error];
-		if (3 == [keys count] || 3 == [values count])
+		// ****************************************************************************************
+		
+		if( [[httpServerMessage valueForKey: @"MethodName"] isEqualToString: @"updateDICOMNode"])	//AETitle, Port, TransferSyntax
 		{
-			int i;
-			NSMutableDictionary *paramDict = [NSMutableDictionary dictionary];
-			for( i = 0; i < [keys count]; i++)
-				[paramDict setValue: [[values objectAtIndex: i] objectValue] forKey: [[keys objectAtIndex: i] objectValue]];
+			NSXMLDocument *doc = [httpServerMessage valueForKey:@"NSXMLDocument"];						// We need the parameters
 			
-			NSLog( @"%@", paramDict);
-			
-			NSMutableArray *serversArray = [NSMutableArray arrayWithArray: [[NSUserDefaults standardUserDefaults] arrayForKey: @"SERVERS"]];
-			
-			BOOL found = NO;
-			
-			for( NSDictionary *d in serversArray)
+			NSError	*error = 0L;
+			NSArray *keys = [doc nodesForXPath:@"methodCall/params//member/name" error:&error];
+			NSArray *values = [doc nodesForXPath:@"methodCall/params//member/value" error:&error];
+			if (3 == [keys count] || 3 == [values count])
 			{
-				if( [[d valueForKey:@"AETitle"] isEqualToString: [paramDict valueForKey:@"AETitle"]])
+				int i;
+				NSMutableDictionary *paramDict = [NSMutableDictionary dictionary];
+				for( i = 0; i < [keys count]; i++)
+					[paramDict setValue: [[values objectAtIndex: i] objectValue] forKey: [[keys objectAtIndex: i] objectValue]];
+				
+				NSLog( @"%@", paramDict);
+				
+				NSMutableArray *serversArray = [NSMutableArray arrayWithArray: [[NSUserDefaults standardUserDefaults] arrayForKey: @"SERVERS"]];
+				
+				BOOL found = NO;
+				
+				for( NSDictionary *d in serversArray)
 				{
-					NSMutableDictionary *m = [NSMutableDictionary dictionaryWithDictionary: d];
-					
-					[m setObject: [httpServerMessage valueForKey: @"peerAddress"] forKey:@"Address"];
-					[m setObject: [paramDict valueForKey:@"Port"] forKey:@"Port"];
-					[m setObject: [NSNumber numberWithInt: [[paramDict valueForKey:@"TransferSyntax"] intValue]] forKey:@"Transfer Syntax"];
-					
-					[serversArray removeObject: d];
-					[serversArray addObject: m];
-					
-					found = YES;
-					
-					break;
+					if( [[d valueForKey:@"AETitle"] isEqualToString: [paramDict valueForKey:@"AETitle"]])
+					{
+						NSMutableDictionary *m = [NSMutableDictionary dictionaryWithDictionary: d];
+						
+						[m setObject: [httpServerMessage valueForKey: @"peerAddress"] forKey:@"Address"];
+						[m setObject: [paramDict valueForKey:@"Port"] forKey:@"Port"];
+						[m setObject: [NSNumber numberWithInt: [[paramDict valueForKey:@"TransferSyntax"] intValue]] forKey:@"Transfer Syntax"];
+						
+						[serversArray removeObject: d];
+						[serversArray addObject: m];
+						
+						found = YES;
+						
+						break;
+					}
 				}
-			}
-			
-			if( found == NO)
-			{
-				[serversArray addObject: [NSDictionary dictionaryWithObjectsAndKeys:	[httpServerMessage valueForKey: @"peerAddress"], @"Address",
-																						[paramDict valueForKey:@"AETitle"], @"AETitle",
-																						[paramDict valueForKey:@"Port"], @"Port",
-																						[NSNumber numberWithBool:YES] , @"QR",
-																						[NSNumber numberWithBool:YES] , @"Send",
-																						[paramDict valueForKey:@"AETitle"], @"Description",
-																						[NSNumber numberWithInt: [[paramDict valueForKey:@"TransferSyntax"] intValue]], @"Transfer Syntax",
-																						nil]];
-			}
-			
-			NSLog( @"%@", serversArray);
-			
-			[[NSUserDefaults standardUserDefaults] setObject: serversArray forKey:@"SERVERS"];
-			
-			// Done, we can send the response to the sender
-			
-			NSString *xml = @"<?xml version=\"1.0\"?><methodResponse><params><param><value><struct><member><name>error</name><value>0</value></member></struct></value></param></params></methodResponse>";		// Simple answer, no errors
-			NSError *error = nil;
-			NSXMLDocument *doc = [[[NSXMLDocument alloc] initWithXMLString:xml options:NSXMLNodeOptionsNone error:&error] autorelease];
-			[httpServerMessage setValue: doc forKey: @"NSXMLDocumentResponse"];
-			[httpServerMessage setValue: [NSNumber numberWithBool: YES] forKey: @"Processed"];		// To tell to other XML-RPC that we processed this order
-		}
-	}
-
-	// ****************************************************************************************
-	
-	if( [[httpServerMessage valueForKey: @"MethodName"] isEqualToString: @"importFromURL"])
-	{
-		NSXMLDocument *doc = [httpServerMessage valueForKey:@"NSXMLDocument"];						// We need the parameters
-		
-		NSError	*error = 0L;
-		NSArray *keys = [doc nodesForXPath:@"methodCall/params//member/name" error:&error];
-		NSArray *values = [doc nodesForXPath:@"methodCall/params//member/value" error:&error];
-		if (1 == [keys count] || 1 == [values count])
-		{
-			int i;
-			NSMutableDictionary *paramDict = [NSMutableDictionary dictionary];
-			for( i = 0; i < [keys count]; i++)
-				[paramDict setValue: [[values objectAtIndex: i] objectValue] forKey: [[keys objectAtIndex: i] objectValue]];
-			
-			// Ok, now, we have the parameters -> execute it !
-			
-//			NSArray *result = [[BrowserController currentBrowser] addURLToDatabaseFiles: [NSArray arrayWithObject: [NSURL URLWithString: @"http://www.osirix-viewer.com/internet.dcm"]]];
-			NSArray *result = [[BrowserController currentBrowser] addURLToDatabaseFiles: [NSArray arrayWithObject: [NSURL URLWithString: [paramDict valueForKey:@"url"]]]];
-			
-			if( [result count] == 0) NSLog(@"error.... addURLToDatabaseFiles failed");
-			
-			// Done, we can send the response to the sender
-			
-			NSString *xml = @"<?xml version=\"1.0\"?><methodResponse><params><param><value><struct><member><name>error</name><value>0</value></member></struct></value></param></params></methodResponse>";		// Simple answer, no errors
-			NSError *error = nil;
-			NSXMLDocument *doc = [[[NSXMLDocument alloc] initWithXMLString:xml options:NSXMLNodeOptionsNone error:&error] autorelease];
-			[httpServerMessage setValue: doc forKey: @"NSXMLDocumentResponse"];
-			[httpServerMessage setValue: [NSNumber numberWithBool: YES] forKey: @"Processed"];		// To tell to other XML-RPC that we processed this order
-		}
-	}
-	
-	// ****************************************************************************************
-	
-	if( [[httpServerMessage valueForKey: @"MethodName"] isEqualToString: @"exportSelectedToPath"])
-	{
-		NSXMLDocument *doc = [httpServerMessage valueForKey:@"NSXMLDocument"];						// We need the parameters
-		
-		NSError	*error = 0L;
-		NSArray *keys = [doc nodesForXPath:@"methodCall/params//member/name" error:&error];
-		NSArray *values = [doc nodesForXPath:@"methodCall/params//member/value" error:&error];
-		if (1 == [keys count] || 1 == [values count])
-		{
-			int i;
-			NSMutableDictionary *paramDict = [NSMutableDictionary dictionary];
-			for( i = 0; i < [keys count]; i++)
-				[paramDict setValue: [[values objectAtIndex: i] objectValue] forKey: [[keys objectAtIndex: i] objectValue]];
-			
-			// Ok, now, we have the parameters -> execute it !
-			
-			NSMutableArray *dicomFiles2Export = [NSMutableArray array];
-			NSMutableArray *filesToExport;
-			
-			filesToExport = [[BrowserController currentBrowser] filesForDatabaseOutlineSelection: dicomFiles2Export onlyImages:YES];
-			[[BrowserController currentBrowser] exportDICOMFileInt: [paramDict valueForKey:@"path"] files: filesToExport objects: dicomFiles2Export];
-			
-			// Done, we can send the response to the sender
-			
-			NSString *xml = @"<?xml version=\"1.0\"?><methodResponse><params><param><value><struct><member><name>error</name><value>0</value></member></struct></value></param></params></methodResponse>";		// Simple answer, no errors
-			NSError *error = nil;
-			NSXMLDocument *doc = [[[NSXMLDocument alloc] initWithXMLString:xml options:NSXMLNodeOptionsNone error:&error] autorelease];
-			[httpServerMessage setValue: doc forKey: @"NSXMLDocumentResponse"];
-			[httpServerMessage setValue: [NSNumber numberWithBool: YES] forKey: @"Processed"];		// To tell to other XML-RPC that we processed this order
-		}
-	}
-
-	
-	// ****************************************************************************************
-	
-	if( [[httpServerMessage valueForKey: @"MethodName"] isEqualToString: @"openSelectedWithTiling"])
-	{
-		NSXMLDocument *doc = [httpServerMessage valueForKey:@"NSXMLDocument"];						// We need the parameters
-		
-		NSError	*error = 0L;
-		NSArray *keys = [doc nodesForXPath:@"methodCall/params//member/name" error:&error];
-		NSArray *values = [doc nodesForXPath:@"methodCall/params//member/value" error:&error];
-		if (2 == [keys count] || 2 == [values count])
-		{
-			int i;
-			NSMutableDictionary *paramDict = [NSMutableDictionary dictionary];
-			for( i = 0; i < [keys count]; i++)
-				[paramDict setValue: [[values objectAtIndex: i] objectValue] forKey: [[keys objectAtIndex: i] objectValue]];
-			
-			// Ok, now, we have the parameters -> execute it !
-			
-			[[BrowserController currentBrowser] viewerDICOM: self];
-			
-			// And change the tiling, of the frontmost viewer
-			
-			NSMutableArray *viewersList = [ViewerController getDisplayed2DViewers];
-			
-			for( i = 0; i < [viewersList count] ; i++)
-			{
+				
+				if( found == NO)
 				{
-					[[viewersList objectAtIndex: i] checkEverythingLoaded];
-					[[viewersList objectAtIndex: i] setImageRows: [[paramDict valueForKey: @"rowsTiling"] intValue] columns: [[paramDict valueForKey: @"rowsTiling"] intValue]];
+					[serversArray addObject: [NSDictionary dictionaryWithObjectsAndKeys:	[httpServerMessage valueForKey: @"peerAddress"], @"Address",
+																							[paramDict valueForKey:@"AETitle"], @"AETitle",
+																							[paramDict valueForKey:@"Port"], @"Port",
+																							[NSNumber numberWithBool:YES] , @"QR",
+																							[NSNumber numberWithBool:YES] , @"Send",
+																							[paramDict valueForKey:@"AETitle"], @"Description",
+																							[NSNumber numberWithInt: [[paramDict valueForKey:@"TransferSyntax"] intValue]], @"Transfer Syntax",
+																							nil]];
 				}
+				
+				NSLog( @"%@", serversArray);
+				
+				[[NSUserDefaults standardUserDefaults] setObject: serversArray forKey:@"SERVERS"];
+				
+				// Done, we can send the response to the sender
+				
+				NSString *xml = @"<?xml version=\"1.0\"?><methodResponse><params><param><value><struct><member><name>error</name><value>0</value></member></struct></value></param></params></methodResponse>";		// Simple answer, no errors
+				NSError *error = nil;
+				NSXMLDocument *doc = [[[NSXMLDocument alloc] initWithXMLString:xml options:NSXMLNodeOptionsNone error:&error] autorelease];
+				[httpServerMessage setValue: doc forKey: @"NSXMLDocumentResponse"];
+				[httpServerMessage setValue: [NSNumber numberWithBool: YES] forKey: @"Processed"];		// To tell to other XML-RPC that we processed this order
 			}
-			
-			// Done, we can send the response to the sender
-			
-			NSString *xml = @"<?xml version=\"1.0\"?><methodResponse><params><param><value><struct><member><name>error</name><value>0</value></member></struct></value></param></params></methodResponse>";		// Simple answer, no errors
-			NSError *error = nil;
-			NSXMLDocument *doc = [[[NSXMLDocument alloc] initWithXMLString:xml options:NSXMLNodeOptionsNone error:&error] autorelease];
-			[httpServerMessage setValue: doc forKey: @"NSXMLDocumentResponse"];
-			[httpServerMessage setValue: [NSNumber numberWithBool: YES] forKey: @"Processed"];		// To tell to other XML-RPC that we processed this order
 		}
+
+		// ****************************************************************************************
+		
+		if( [[httpServerMessage valueForKey: @"MethodName"] isEqualToString: @"importFromURL"])
+		{
+			NSXMLDocument *doc = [httpServerMessage valueForKey:@"NSXMLDocument"];						// We need the parameters
+			
+			NSError	*error = 0L;
+			NSArray *keys = [doc nodesForXPath:@"methodCall/params//member/name" error:&error];
+			NSArray *values = [doc nodesForXPath:@"methodCall/params//member/value" error:&error];
+			if (1 == [keys count] || 1 == [values count])
+			{
+				int i;
+				NSMutableDictionary *paramDict = [NSMutableDictionary dictionary];
+				for( i = 0; i < [keys count]; i++)
+					[paramDict setValue: [[values objectAtIndex: i] objectValue] forKey: [[keys objectAtIndex: i] objectValue]];
+				
+				// Ok, now, we have the parameters -> execute it !
+				
+	//			NSArray *result = [[BrowserController currentBrowser] addURLToDatabaseFiles: [NSArray arrayWithObject: [NSURL URLWithString: @"http://www.osirix-viewer.com/internet.dcm"]]];
+				
+				NSString *url = [paramDict valueForKey:@"url"];
+				if( url == 0L) url = [paramDict valueForKey:@"url "];
+				if( url == 0L) url = [paramDict valueForKey:@"URL"];
+				
+				if( url)
+				{
+					NSArray *result = [[BrowserController currentBrowser] addURLToDatabaseFiles: [NSArray arrayWithObject: [NSURL URLWithString: url]]];
+					if( [result count] == 0) NSLog(@"error.... addURLToDatabaseFiles failed");
+				}
+				else NSLog( @"no URL ! for importFromURL");
+				
+				// Done, we can send the response to the sender
+				
+				NSString *xml = @"<?xml version=\"1.0\"?><methodResponse><params><param><value><struct><member><name>error</name><value>0</value></member></struct></value></param></params></methodResponse>";		// Simple answer, no errors
+				NSError *error = nil;
+				NSXMLDocument *doc = [[[NSXMLDocument alloc] initWithXMLString:xml options:NSXMLNodeOptionsNone error:&error] autorelease];
+				[httpServerMessage setValue: doc forKey: @"NSXMLDocumentResponse"];
+				[httpServerMessage setValue: [NSNumber numberWithBool: YES] forKey: @"Processed"];		// To tell to other XML-RPC that we processed this order
+			}
+		}
+		
+		// ****************************************************************************************
+		
+		if( [[httpServerMessage valueForKey: @"MethodName"] isEqualToString: @"exportSelectedToPath"])
+		{
+			NSXMLDocument *doc = [httpServerMessage valueForKey:@"NSXMLDocument"];						// We need the parameters
+			
+			NSError	*error = 0L;
+			NSArray *keys = [doc nodesForXPath:@"methodCall/params//member/name" error:&error];
+			NSArray *values = [doc nodesForXPath:@"methodCall/params//member/value" error:&error];
+			if (1 == [keys count] || 1 == [values count])
+			{
+				int i;
+				NSMutableDictionary *paramDict = [NSMutableDictionary dictionary];
+				for( i = 0; i < [keys count]; i++)
+					[paramDict setValue: [[values objectAtIndex: i] objectValue] forKey: [[keys objectAtIndex: i] objectValue]];
+				
+				// Ok, now, we have the parameters -> execute it !
+				
+				NSMutableArray *dicomFiles2Export = [NSMutableArray array];
+				NSMutableArray *filesToExport;
+				
+				filesToExport = [[BrowserController currentBrowser] filesForDatabaseOutlineSelection: dicomFiles2Export onlyImages:YES];
+				[[BrowserController currentBrowser] exportDICOMFileInt: [paramDict valueForKey:@"path"] files: filesToExport objects: dicomFiles2Export];
+				
+				// Done, we can send the response to the sender
+				
+				NSString *xml = @"<?xml version=\"1.0\"?><methodResponse><params><param><value><struct><member><name>error</name><value>0</value></member></struct></value></param></params></methodResponse>";		// Simple answer, no errors
+				NSError *error = nil;
+				NSXMLDocument *doc = [[[NSXMLDocument alloc] initWithXMLString:xml options:NSXMLNodeOptionsNone error:&error] autorelease];
+				[httpServerMessage setValue: doc forKey: @"NSXMLDocumentResponse"];
+				[httpServerMessage setValue: [NSNumber numberWithBool: YES] forKey: @"Processed"];		// To tell to other XML-RPC that we processed this order
+			}
+		}
+
+		
+		// ****************************************************************************************
+		
+		if( [[httpServerMessage valueForKey: @"MethodName"] isEqualToString: @"openSelectedWithTiling"])
+		{
+			NSXMLDocument *doc = [httpServerMessage valueForKey:@"NSXMLDocument"];						// We need the parameters
+			
+			NSError	*error = 0L;
+			NSArray *keys = [doc nodesForXPath:@"methodCall/params//member/name" error:&error];
+			NSArray *values = [doc nodesForXPath:@"methodCall/params//member/value" error:&error];
+			if (2 == [keys count] || 2 == [values count])
+			{
+				int i;
+				NSMutableDictionary *paramDict = [NSMutableDictionary dictionary];
+				for( i = 0; i < [keys count]; i++)
+					[paramDict setValue: [[values objectAtIndex: i] objectValue] forKey: [[keys objectAtIndex: i] objectValue]];
+				
+				// Ok, now, we have the parameters -> execute it !
+				
+				[[BrowserController currentBrowser] viewerDICOM: self];
+				
+				// And change the tiling, of the frontmost viewer
+				
+				NSMutableArray *viewersList = [ViewerController getDisplayed2DViewers];
+				
+				for( i = 0; i < [viewersList count] ; i++)
+				{
+					{
+						[[viewersList objectAtIndex: i] checkEverythingLoaded];
+						[[viewersList objectAtIndex: i] setImageRows: [[paramDict valueForKey: @"rowsTiling"] intValue] columns: [[paramDict valueForKey: @"rowsTiling"] intValue]];
+					}
+				}
+				
+				// Done, we can send the response to the sender
+				
+				NSString *xml = @"<?xml version=\"1.0\"?><methodResponse><params><param><value><struct><member><name>error</name><value>0</value></member></struct></value></param></params></methodResponse>";		// Simple answer, no errors
+				NSError *error = nil;
+				NSXMLDocument *doc = [[[NSXMLDocument alloc] initWithXMLString:xml options:NSXMLNodeOptionsNone error:&error] autorelease];
+				[httpServerMessage setValue: doc forKey: @"NSXMLDocumentResponse"];
+				[httpServerMessage setValue: [NSNumber numberWithBool: YES] forKey: @"Processed"];		// To tell to other XML-RPC that we processed this order
+			}
+		}
+		
+		// ****************************************************************************************
+
+		NSLog( [httpServerMessage description]);
 	}
 	
-	// ****************************************************************************************
-
-	NSLog( [httpServerMessage description]);
+	@catch (NSException *e)
+	{
+		NSLog( @"**** EXCEPTION IN XML-RPC PROCESSING: %@", e);
+	}
 }
 
 - (long) filterImage : (NSString*) menuName
