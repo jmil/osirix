c2c4e48e0b69b99393785b933095700ce13306e9
rossetantoine <rossetantoine@d300eccd-f00d-0410-ae38-fa88e4616176>
2009-06-06 12:44:51 +0000

    copyright url
    git-svn-id: https://osirix.svn.sourceforge.net/svnroot/osirix@5620 d300eccd-f00d-0410-ae38-fa88e4616176

--- plugins/CMIV_CTA_TOOLS/VRMakeObject.h
+++ plugins/CMIV_CTA_TOOLS/VRMakeObject.h
@@ -5,7 +5,7 @@
   All rights reserved.
   Distributed under GNU - GPL
   
-  See http://homepage.mac.com/rossetantoine/osirix/copyright.html for details.
+  See http://www.osirix-viewer.com/copyright.html for details.
 
      This software is distributed WITHOUT ANY WARRANTY; without even
      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
@@ -79,7 +79,7 @@
 //	Written by:	Tim Monroe
 //				Based on MakeQTVRObject code by Pete Falco and Michael Chen (and others?).
 //
-//	Copyright:	© 1991-1998 by Apple Computer, Inc., all rights reserved.
+//	Copyright:	ï¿½ 1991-1998 by Apple Computer, Inc., all rights reserved.
 //
 //	Change History (most recent first):
 //
