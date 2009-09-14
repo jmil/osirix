639186615ef3e82d1792499005c671e1ef0f77df
al3ss <al3ss@d300eccd-f00d-0410-ae38-fa88e4616176>
2009-09-14 11:02:12 +0000

    [NSColor isEqualToColor:] fixed
    git-svn-id: https://osirix.svn.sourceforge.net/svnroot/osirix@6015 d300eccd-f00d-0410-ae38-fa88e4616176

--- nitrogen/Sources/NSColor+N2.mm
+++ nitrogen/Sources/NSColor+N2.mm
@@ -26,10 +26,16 @@
 	
 	NSInteger numberOfComponents = [c1 numberOfComponents];
 	CGFloat c1components[numberOfComponents], c2components[numberOfComponents];
+	[c1 getComponents:c1components]; [c2 getComponents:c2components];
 	
-	for (NSInteger i = 0; i < numberOfComponents; ++i)
+	if (!c1components[numberOfComponents-1] || !c2components[numberOfComponents-1])
+		return YES;
+	
+	for (NSInteger i = 0; i < numberOfComponents-1; ++i) {
+		NSLog(@"component %d: %f, %f", i, c1components[i], c2components[i]);
 		if (c1components[i] != c2components[i])
 			return NO;
+	}
 	
 	return YES;
 }
