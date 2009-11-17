1c4b67a303451ea03455015f15eed05c036c06a4
rossetantoine <rossetantoine@d300eccd-f00d-0410-ae38-fa88e4616176>
2009-11-17 20:44:45 +0000

    BullsEye plugin
    git-svn-id: https://osirix.svn.sourceforge.net/svnroot/osirix@6241 d300eccd-f00d-0410-ae38-fa88e4616176

---
+++ plugins/BullsEye/BullsEye.m
@@ -0,0 +1,21 @@
+//
+//  BullsEye.m
+//  BullsEye
+//
+//  Copyright (c) 2009 Antoine Rosset. All rights reserved.
+//
+
+#import "BullsEye.h"
+#import "OsiriX/DCMObject.h"
+#import "OsiriX/DCMTransferSyntax.h"
+#import "DCMPix.h"
+
+@implementation BullsEye
+
+- (long) filterImage:(NSString*) menuName
+{
+	
+	return 0;   // No Errors
+}
+
+@end
